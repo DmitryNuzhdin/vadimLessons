9a727e04707d57db9515aabbd7dda66f950d524e
DmitryNuzhdin <mr.dmitrynuzhdin@yandex.ru>
2020-04-24 14:53:40 +0300

    ValidParentheses example solution

--- src/main/java/tasks/collections/ValidParentheses.java
+++ src/main/java/tasks/collections/ValidParentheses.java
@@ -1,5 +1,10 @@
 package tasks.collections;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
 public class ValidParentheses {
     public static boolean checkValidness(String combination) {
         // довольно классическая задача
@@ -20,7 +25,74 @@ public class ValidParentheses {
 
         // (*) Сделать так, чтобы добавить новый вид скобок можно было максимально просто и понятно.
 
+        List<ParenthesisPair> types = new ArrayList<>(); //список пар скобок которые знает программа
+        types.add(new ParenthesisPair('(', ')'));
+        types.add(new ParenthesisPair('[', ']'));
+        types.add(new ParenthesisPair('{', '}'));
+        //можно легко в одну строчку добавить новый тип
+
+        LinkedList<Character> chars = new LinkedList<>();
+        for (char c:combination.toCharArray()) {
+            chars.add(c);
+        }
+        if (chars.size() == 0) return true;
+        while (deletePairs(chars, types)) {
+            if (chars.size() == 0) return true;
+        }  // удаляем пары до тех пор пока они удаляются
 
         return false;
+    }
+
+    private static boolean deletePairs(LinkedList<Character> list, List<ParenthesisPair> types) {
+        //метод удаляет пары, и возвращает true если что-то было удалено
+        boolean answer = false;  //изначально ответ отрицательный
+        for (int i=0; i < list.size() - 1; i++) {
+            ParenthesisPair pair = new ParenthesisPair(list.get(i), list.get(i + 1));  //собираем пару из скобок с индексами i и i + 1
+            // на самом деле здесь есть проблема - list.get() в LinkedList выполняется за O(n)
+            // ArrayList в свою очередь имеет O(n) для .remove(), что тоже плохо
+            // поэтому такое решение будет супер медленное наверное в итоге получится вообще O(n^3) что очень плохо
+            // но сейчас речь о другом, в идеале алгоритм должен быть немного другим в принципе
+            for (ParenthesisPair type: types){
+                if (type.equals(pair)) {  //если здесь сравнивать через == , то работать не будет (попробуй понять почему)
+                    list.remove(i);
+                    list.remove(i);
+                    answer = true;   //отмечаем что было удаление
+                }
+            }
+        }
+        return answer;
+    }
+}
+
+class ParenthesisPair {
+    // простой класс - контейнр 2 величин с реализованным (сгенеренным автоматически через alt-insert) методом equals
+    // это "тип данных" - пара скобок, благодаря ему можно сравнивать сразу пары, уменьшить количество математики в коде типа
+    // (listA.get(i) == listB.get(k) && listA.get(i+1) == listB.get(k+1))
+    // превращается в
+    // (pairA.equals(pairB))
+    // существенно улучшает читаемость кода, единственный минус - создание этого класса - на Java он громоздкий
+    // к сожалению Java всегда останется "многословной", но вот ее наследники такие как Scala или Kotlin эту проблему решают специальными конструкциями
+    // например аналогичный класс на Scala, с тем-же функционалом (даже немного больше) реализуется в 1 строчку
+    // case class ParenthesisPair(left: char, right:char)
+    final char left;
+    final char right;
+
+    public ParenthesisPair(char left, char right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ParenthesisPair that = (ParenthesisPair) o;
+        return left == that.left &&
+                right == that.right;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(left, right);
     }
 }
